e3f7f88d2cd1c6879a903a17192d2db82d2a891e
AlisaSujyan <a_sujyan@mail.ru>
2024-09-29 23:00:10 +0400

    Bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -1,5 +1,5 @@
 def bubble_sort(A):
-    for i in range(len(A)):
+    for i in range(len(A) - 1):
         for j in range((i + 1), len(A)):
             if A[j] < A[i]:
                 A[i],A[j] = A[j],A[i]
